d31977bd0f294e4aa4464984680c8eeb66deb8d8
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-08-13 14:04:15 +0500

    reverse

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -19,6 +19,21 @@ public class ListNode {
     }
 }
 
+func reverseLinkedList(_ head: ListNode?) -> ListNode? {
+    var prev: ListNode? = nil
+    var current = head
+    
+    while current != nil {
+        let next = current?.next
+        current?.next = prev
+        prev = current
+        current = next
+    }
+    return prev
+}
+
+
+
 ///Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
 //Input: head = [1,1,2]
 //Output: [1,2]
@@ -33,3 +48,23 @@ func deleteDuplicates(head: ListNode?) -> ListNode? {
     }
     return head
 }
+
+func printLinkedList(head: ListNode?) {
+    var output = ""
+    var currentNode = head
+    while currentNode != nil {
+        output += String(currentNode?.val ?? 0)
+        currentNode = currentNode?.next
+    }
+    print(output)
+}
+
+let second = ListNode(1)
+let first = ListNode(1, second)
+let head = ListNode(0, first)
+printLinkedList(head: head)
+//011
+
+deleteDuplicates(head: head)
+printLinkedList(head: head)
+//01
