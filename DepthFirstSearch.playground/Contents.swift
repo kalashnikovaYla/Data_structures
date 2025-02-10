426bba03753ae12782e91a0e876347e818c2912c
Abo <abo@Abos-Mac-Studio.local>
2025-02-10 15:17:45 +0500

    add

--- DepthFirstSearch.playground/Contents.swift
+++ DepthFirstSearch.playground/Contents.swift
@@ -9,15 +9,16 @@ class Graph {
         adjacencyList[v, default: []].append(u)
     }
     
-    func bfs(start: Int) {
+    func bfs(start: Int) -> [Int] {
         var visited = Set<Int>()
         var queue: [Int] = [start]
+        var res: [Int] = []
         
         visited.insert(start)
         
         while !queue.isEmpty {
             let vertex = queue.removeFirst()
-            print(vertex)
+            res.append(vertex)
             
             for neighbor in adjacencyList[vertex, default: []] {
                 if !visited.contains(neighbor) {
@@ -26,24 +27,27 @@ class Graph {
                 }
             }
         }
+        return res
     }
     
-    func dfs(start: Int) {
+    func dfs(start: Int) -> [Int] {
         var visited = Set<Int>()
-        dfsUtil(vertex: start, visited: &visited)
-    }
-       
-    private func dfsUtil(vertex: Int, visited: inout Set<Int>) {
-        visited.insert(vertex)
-        print(vertex)
+        var result = [Int]()
+        dfsUtil(vertex: start, visited: &visited, result: &result)
         
+        return result
+    }
+
+    private func dfsUtil(vertex: Int, visited: inout Set<Int>, result: inout [Int]) {
+        visited.insert(vertex)
+        result.append(vertex)
+
         for neighbor in adjacencyList[vertex, default: []] {
             if !visited.contains(neighbor) {
-                dfsUtil(vertex: neighbor, visited: &visited)
+                dfsUtil(vertex: neighbor, visited: &visited, result: &result)
             }
         }
     }
-
 }
 
 
@@ -53,6 +57,5 @@ graph.addEdge(1, 3)
 graph.addEdge(2, 4)
 graph.addEdge(3, 5)
 
-print("Обход в ширину (BFS):")
-
-graph.bfs(start: 1)
+ 
+print(graph.bfs(start: 1))
