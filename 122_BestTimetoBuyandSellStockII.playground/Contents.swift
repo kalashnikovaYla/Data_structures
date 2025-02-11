6338a0581ff2c6695c13e789328cd338403c8152
Abo <abo@Abos-Mac-Studio.local>
2025-02-11 13:09:58 +0500

    Course Schedule

--- 122_BestTimetoBuyandSellStockII.playground/Contents.swift
+++ 122_BestTimetoBuyandSellStockII.playground/Contents.swift
@@ -11,16 +11,15 @@ import UIKit
 
 class Solution {
     func maxProfit(_ prices: [Int]) -> Int {
-         var totalProfit = 0
-    
-    for i in 1..<prices.count {
-       
-        if prices[i] > prices[i - 1] {
-            totalProfit += prices[i] - prices[i - 1]
+        var totalProfit = 0
+        
+        for i in 1..<prices.count {
+            
+            if prices[i] > prices[i - 1] {
+                totalProfit += prices[i] - prices[i - 1]
+            }
         }
-    }
-    
-    return totalProfit
-
+        
+        return totalProfit
     }
 }
--- Graph.playground/Contents.swift
+++ Graph.playground/Contents.swift
@@ -48,7 +48,7 @@ class Graph {
     }
 }
 
-// Пример использования
+
 let graph = Graph()
 graph.addEdge(from: "A", to: "B", weight: 1)
 graph.addEdge(from: "A", to: "C", weight: 4)
@@ -58,3 +58,83 @@ graph.addEdge(from: "C", to: "D", weight: 1)
 
 let distances = graph.dijkstra(start: "A")
 print(distances)
+
+
+ 
+///207. Course Schedule
+///There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+///For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+///Return true if you can finish all courses. Otherwise, return false.
+
+
+func canFinish(_ numCourses: Int, _ prerequisites: [[Int]]) -> Bool {
+    var graph = Array(repeating: [Int](), count: numCourses)
+    var visited = Array(repeating: 0, count: numCourses)
+    
+    for prereq in prerequisites {
+        let course = prereq[0]
+        let preCourse = prereq[1]
+        graph[preCourse].append(course)
+    }
+    
+    for course in 0..<numCourses {
+        if !dfs(course, &visited, graph) {
+            return false
+        }
+    }
+    
+    return true
+}
+    
+private func dfs(_ course: Int, _ visited: inout [Int], _ graph: [[Int]]) -> Bool {
+    if visited[course] == 1 {
+        return false
+    }
+    
+    if visited[course] == 2 {
+        return true
+    }
+    
+    visited[course] = 1
+    
+    for neighbor in graph[course] {
+        if !dfs(neighbor, &visited, graph) {
+            return false
+        }
+    }
+    
+    visited[course] = 2
+    return true
+}
+ 
+class Solution {
+    func canFinish(_ numCourses: Int, _ prerequisites: [[Int]]) -> Bool {
+        var graph = [Int:[Int]](), inDegrees = [Int](), sources = [Int]()
+        //Init
+        for i in 0..<numCourses {
+            graph[i] = [Int]()
+            inDegrees.append(0)
+        }
+        //Fill
+        for p in prerequisites {
+            let par = p[1], c = p[0]
+            graph[par]!.append(c)
+            inDegrees[c] += 1
+        }
+
+        //Get sources
+        for i in 0..<inDegrees.count {
+            if inDegrees[i] == 0 { sources.append(i) }
+        }
+        var count = 0
+        while sources.count > 0 {
+            let top = sources.removeLast()
+            count += 1
+            for n in graph[top]! {
+                inDegrees[n] -= 1
+                if inDegrees[n] == 0 { sources.append(n) }
+            }
+        }
+        return count == numCourses
+    }
+}
